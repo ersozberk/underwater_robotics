4913a9c2a5575e2d7808d8efac9ec39682c12dc5
Berk Ersoz <67341010+ersozberk@users.noreply.github.com>
2024-06-04 18:57:37 +0300

    Update autonomous.py

---
+++ src/rov/autonomous.py
@@ -0,0 +1,5 @@
+import time
+
+def autonomous_driving(): print("Starting autonomous driving mode...") time.sleep(2) print("Driving towards the destination") time.sleep(5) print("Avoiding obstacles") time.sleep(3) print("Arrived at the destination. Autonomous driving mode completed.")
+
+autonomous_driving()
